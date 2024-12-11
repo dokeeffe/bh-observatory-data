aafc4594449b2060cd1e898f74f495b355e51fe5
Derek <derek0207@gmail.com>
2024-12-11 19:44:10 +0000

    pc backup

--- datasets/sky_quality_meter/SQM_LU/sqm-data-extract.py
+++ datasets/sky_quality_meter/SQM_LU/sqm-data-extract.py
@@ -5,19 +5,20 @@ import pandas as pd
 import os
 from astropy.io import fits
 
-# for root, dirnames, filenames in os.walk('/home/dokeeffe/Pictures/CalibratedLight'):
+fits_dirs = ['/home/dokeeffe/Pictures/CalibratedLight','/home/dokeeffe/Pictures/CalibratedLightArchive']
 data = []
-for root, dirnames, filenames in os.walk('/home/dokeeffe/Pictures/CalibratedLight'):
-    for filename in fnmatch.filter(filenames, '*.fits'):
-        try:
-            hdulist = fits.open(os.path.join(root, filename))
-            header = hdulist[0].header
-            if 'MPSAS' in header:
-                sqm = header['MPSAS']
-                date_obs = header['DATE-OBS']
-                data.append([date_obs,sqm])
-        except:
-            print(f'Could not open {filename}')
+for fits_dir in fits_dirs:
+    for root, dirnames, filenames in os.walk(fits_dir):
+        for filename in fnmatch.filter(filenames, '*.fits'):
+            try:
+                hdulist = fits.open(os.path.join(root, filename))
+                header = hdulist[0].header
+                if 'MPSAS' in header:
+                    sqm = header['MPSAS']
+                    date_obs = header['DATE-OBS']
+                    data.append([date_obs,sqm])
+            except:
+                print(f'Could not open {filename}')
 
 df = pd.DataFrame(data, columns=['datetime_obs', 'mpsas'])
 df = df.set_index('datetime_obs')
